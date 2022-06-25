1c5e6cb57155f138f0d95eff26d313647c5ff31c
Milo_Lynch <Milobravman20@gmail.com>
2022-06-25 17:15:29 -0400

    the problem with this is i need to actally make the listnode class

--- Add_two_nums/addTwoNums.java
+++ Add_two_nums/addTwoNums.java
@@ -1,11 +1,15 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode head = new ListNode(0);
+
+        System.out.print(l1);
         
         String l1_As_str= "";
         String l2_As_str= "";
         while (l1 != null){
             int value = l1.val;
+            System.out.println(value);
+            System.out.println("value");
             String value_as_str = String.valueOf(value) ;
             //head.val = x;
             l1 = l1.next;
@@ -14,6 +18,9 @@ class Solution {
     }
     public static void main(String[] args){
         Solution k = new Solution();
+        ListNode x = new ListNode();
+        ListNode y = new ListNode();
+        k.addTwoNumbers()
         System.out.println(k);
     }
 }
