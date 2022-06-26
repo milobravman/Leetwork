25fb2ffa75e5b85192d9f015b5e55f357f5505bb
Milo_Lynch <Milobravman20@gmail.com>
2022-06-26 15:51:51 -0400

    I didnt understand line 10 its adding everthing to the charters to the front and starts with H

--- Add_two_nums/ReversingStr.java
+++ Add_two_nums/ReversingStr.java
@@ -5,8 +5,9 @@ class Reverse {
         for (int i=0; i<h.length(); i++)
         {
              char ch= h.charAt(i); //extracts each character
-             System.out.println(ch);
+             //System.out.println(ch);
              r = ch+r; //adds each character in front of the existing string
+             System.out.println(r);
         }
         System.out.println(r);
     }
