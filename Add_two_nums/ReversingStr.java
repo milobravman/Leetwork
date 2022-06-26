219c326359cba18993f29d59e78b8fec376e2d40
Milo_Lynch <Milobravman20@gmail.com>
2022-06-26 15:49:36 -0400

    trying to understand how reverse method works]

---
+++ Add_two_nums/ReversingStr.java
@@ -0,0 +1,13 @@
+class Reverse {
+    public static void main(String[] args){
+        String h = "Hello";
+        String r = "";
+        for (int i=0; i<h.length(); i++)
+        {
+             char ch= h.charAt(i); //extracts each character
+             System.out.println(ch);
+             r = ch+r; //adds each character in front of the existing string
+        }
+        System.out.println(r);
+    }
+}
