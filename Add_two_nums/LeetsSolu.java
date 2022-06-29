21ab183a48c11f9ac4c33fc6166ce30d5fb44807
Milo_Lynch <Milobravman20@gmail.com>
2022-06-29 01:42:01 -0400

    okay ? in java are cool they probobly come up in other languages to they are called conditional question mark or ternary operators

---
+++ Add_two_nums/LeetsSolu.java
@@ -0,0 +1,28 @@
+// public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//     ListNode dummyHead = new ListNode(0);
+//     ListNode p = l1, q = l2, curr = dummyHead;
+//     int carry = 0;
+//     while (p != null || q != null) {
+//         int x = (p != null) ? p.val : 0;
+//         int y = (q != null) ? q.val : 0;
+//         int sum = carry + x + y;
+//         carry = sum / 10;
+//         curr.next = new ListNode(sum % 10);
+//         curr = curr.next;
+//         if (p != null) p = p.next;
+//         if (q != null) q = q.next;
+//     }
+//     if (carry > 0) {
+//         curr.next = new ListNode(carry);
+//     }
+//     return dummyHead.next;
+// }
+
+public class LeetsSolu {
+    public static void main(String[] args) {
+        int l = 10;
+        int p = 9;
+        int k = (l > p) ? 4:5;
+        System.out.println(k);
+    }
+}
